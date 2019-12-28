bd737ccf77a4369ecdcc8553674acf225ad36e0a
hepei <55754680+hp-kiki@users.noreply.github.com>
2019-12-28 17:13:48 +0800

    更改

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    let isboy=true
+    let f=function(msg){
+       return '------'+msg+'--------'
+    }
+    let obj={
+      price:100,
+      size:'small'
+    }
+    return (
+      <div>
+        {
+          isboy?'男':'女'
+         }
+         {
+           f('调用函数')
+          
+         }
+         {
+           obj.price
+         }
+         -----
+         {
+           obj.size
+         }
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+
+
+
+
+// //引入依赖
+// import React,{Component} from "react"
+// //引入css文件
+// import "./App.css"
+
+
+// export default class App extends Component {
+//   render() {
+//     let title=['Pull requests','lssues','Marketplace','Explore']
+//     return (
+//       <div class="test">
+//         <ul>
+//           {
+//             title.map((v,i) => <li key={i}>{v}</li>)
+//           }
+
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+
 
